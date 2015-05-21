469121a7bf726831883bccf812e8dedb548436db
Melissa Wallace <melissakuo@gmail.com>
2015-05-21 16:27:32 -0400

    add visual design to theme files

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -73,96 +73,169 @@
  * @ingroup themeable
  */
 ?>
-<header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
-  <div class="container">
-    <div class="navbar-header">
-      <?php if ($logo): ?>
-      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-      <?php endif; ?>
 
-      <?php if (!empty($site_name)): ?>
-      <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
-
-      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
+<div class="hero">
+  <div class="cornell-identity">
+    <div class="container">
+      <div class="row">
+        <div class="cornell-logo">
+          <a href="http://www.cornell.edu"><img src="/sites/all/themes/base/img/cornell.gif" alt="Cornell University"></a>
+        </div>
+        <div class="global-menu">
+          <div class="navbar">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mobile-nav">
+                <span class="sr-only">Menu</span>
+                <i class="fa fa-bars"></i>
+              </button>
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mobile-search">
+                <span class="sr-only">Search</span>
+                <i class="fa fa-search"></i>
+              </button>
+            </div>
+            <?php if (!empty($global_nav)): ?>
+              <div class="collapse navbar-collapse hidden-xs" id="menu">
+                <?php print render($page['global_nav']); ?>
+                <!-- <ul class="nav navbar-nav main-nav">
+                <li><a href="#">Optional</a></li>
+                <li><a href="#">Global</a></li>
+                <li><a href="#">Level</a></li>
+                <li><a href="#">Navigation</a></li>
+                </ul> -->
+              </div><!-- /.navbar-collapse -->
+            <?php endif; ?>
+          </div>
+        </div>
+      </div>  
     </div>
+  </div>
+  <header>
+    <?php if (!empty($mobile_nav)): ?>
+      <div id="mobile-nav" class="collapse">
+        <?php print render($page['mobile_nav']); ?>
+          <!-- <ul class="nav navbar-nav main-nav">
+          <li><a href="#">Optional</a></li>
+          <li><a href="#">Global</a></li>
+          <li><a href="#">Level</a></li>
+          <li><a href="#">Navigation</a></li>
+          <li><a href="/sub">Menu item</a></li>
+          <li><a href="/sub">Menu item</a></li>
+          <li><a href="/sub">Menu item</a></li>
+          <li><a href="/sub">Menu item</a></li>
+          <li><a href="/sub">Menu item</a></li>
+          <li><a href="/sub">Menu item</a></li>
+        </ul> -->
+      </div>
+    <?php endif; ?>
+    
+    <?php if (!empty($mobile_search)): ?>
+      <div id="mobile-search" class="collapse">
+        <?php print render($page['mobile_search']); ?>
+        <!-- <form class="form-inline clearfix">
+          <div class="form-group">
+            <label for="search" class="sr-only">Search</label>
+            <input type="text" class="form-control" id="search" placeholder="Search">
+          </div>
+          <button type="submit" class="btn btn-search">
+            <span class="sr-only">Search</span>
+            <i class="fa fa-search"></i>
+          </button>
+        </form> -->
+      </div>
+    <?php endif; ?>
+    <div class="container">
+      <div class="row">
+        <div class="library-brand">
+          <a class="cul-brand" href="http://www.library.cornell.edu">Cornell University Library</a>
+          <?php if (!empty($site_name)): ?>
+            <a class="brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+          <?php endif; ?>
+        </div>
+        <?php if (!empty($search)): ?>
+          <div class="header-search hidden-xs">
+            <?php print render($page['search']); ?>
+            <!-- <form class="form-inline">
+              <div class="form-group">
+                <label for="search" class="sr-only">Search</label>
+                <input type="text" class="form-control" id="search" placeholder="Search">
+              </div>
+              <button type="submit" class="btn btn-search">
+                <span class="sr-only">Search</span>
+                <i class="fa fa-search"></i>
+              </button>
+            </form> -->
+          </div>
+        <?php endif; ?>
+      </div>
+    </div>
+  </header>
+  <?php if (!empty($main_nav)): ?>
+    <nav class="navbar hidden-xs">
+      <div class="container">
+        <?php print render($page['main_nav']); ?>
+        <!-- <ul class="nav navbar-nav">
+          <li><a href="/sub">Menu item</a></li>
+          <li><a href="/sub">Menu item</a></li>
+          <li><a href="/sub">Menu item</a></li>
+          <li><a href="/sub">Menu item</a></li>
+          <li><a href="/sub">Menu item</a></li>
+          <li><a href="/sub">Menu item</a></li>
+        </ul> -->
+      </div>
+    </nav>
+  <?php endif; ?>
+</div>
 
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse">
-        <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
+<div class="main-content">
+  <div class="container">
+    <?php if (!empty($page['sidebar'])): ?>
+      <div class="row"> 
+        <div class="main-text">
+          <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+          <?php print render($title_prefix); ?>
+          <?php if (!empty($title)): ?>
+            <h1><?php print $title; ?></h1>
           <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
+          <?php print render($title_suffix); ?>
+          <?php print $messages; ?>
+          <?php if (!empty($tabs)): ?>
+            <?php print render($tabs); ?>
           <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
+          <?php if (!empty($page['help'])): ?>
+            <?php print render($page['help']); ?>
           <?php endif; ?>
-        </nav>
+          <?php if (!empty($action_links)): ?>
+            <ul class="action-links"><?php print render($action_links); ?></ul>
+          <?php endif; ?>
+          <?php print render($page['content']); ?>
+        </div>
+        <div class="sidebar">
+          <?php print render($page['sidebar']); ?>
+          <!-- <h2 class="block-title">Section header</h2>
+          <ul class="nav">
+            <li><a href="#"><i class="fa fa-chevron-right"></i>Sub page</a></li>
+            <li><a href="#"><i class="fa fa-chevron-right"></i>Sub page</a></li>
+            <li><a href="#"><i class="fa fa-chevron-right"></i>Sub page</a></li>
+            <li><a href="#"><i class="fa fa-chevron-right"></i>Sub page</a></li>
+            <li><a href="#"><i class="fa fa-chevron-right"></i>Sub page</a></li>
+          </ul> -->
+        </div>
       </div>
     <?php endif; ?>
   </div>
-</header>
-
-<div class="main-container container">
-
-  <header role="banner" id="page-header">
-    <?php if (!empty($site_slogan)): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
-    <?php endif; ?>
-
-    <?php print render($page['header']); ?>
-  </header> <!-- /#page-header -->
-
-  <div class="row">
-
-    <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_first']); ?>
-      </aside>  <!-- /#sidebar-first -->
-    <?php endif; ?>
-
-    <section<?php print $content_column_class; ?>>
-      <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
-      <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-      <?php endif; ?>
-      <?php if (!empty($page['help'])): ?>
-        <?php print render($page['help']); ?>
-      <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-    </section>
-
-    <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_second']); ?>
-      </aside>  <!-- /#sidebar-second -->
-    <?php endif; ?>
-
-  </div>
 </div>
-<footer class="footer container">
-  <?php print render($page['footer']); ?>
+
+<footer>
+  <div class="container">
+    <?php print render($page['footer']); ?>
+    <!-- <ul class="nav">
+      <li><a href="#">Link</a></li>
+      <li><a href="#">Link</a></li>
+      <li><a href="#">Link</a></li>
+      <li><a href="#">Link</a></li>
+      <li><a href="#">Link</a></li>
+      <li><a href="#">Link</a></li>
+    </ul>
+    <p>&copy; 2015, Cornell University Library | <a href="#">Privacy</a></p> -->
+  </div>
 </footer>
